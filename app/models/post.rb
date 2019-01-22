55c9e4326bdfcf77aec197c938be11adc77f4fd8
Arsgorv <arsgorv@gmail.com>
2019-01-22 15:02:06 +0000

    Modified comment.rb, post.rb, user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, uniqueness: true
+  validates :body, length: {in: 5..1000}
+  has_many :comments
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {minimum: 2, maximum: 20}
   validates :email, uniqueness: true
+  has_many :comments
+  has_many :posts
 end
